f9cf3bdcc9b0d35fc8108113c1433cf42c2fac54
Stiffler-A <styphler17@gmail.com>
2025-06-02 13:38:10 +0200

    Structure VITE-TAILWIND

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss()],
+});
+// This configuration file sets up Vite with Tailwind CSS as a plugin.
